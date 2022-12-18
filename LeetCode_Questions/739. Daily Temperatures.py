12dfa264b7c3cb4a1f29c86fa824fe0095aa1a84
Sagar Sharan <sagarsharan000@gmail.com>
2022-12-18 14:24:26 +0530

    739 LC code base

---
+++ LeetCode_Questions/739. Daily Temperatures.py
@@ -0,0 +1,21 @@
+# Input: temperatures = [73,74,75,71,69,72,76,73]
+# Output: [1,1,4,2,1,1,0,0]
+
+# Input: temperatures = [30,40,50,60]
+# Output: [1,1,1,0]
+
+# Input: temperatures = [30,60,90]
+# Output: [1,1,0]
+
+def dailyTemperatures(temperatures):
+    pass
+
+if __name__=="__main__":
+    temperatures = [73,74,75,71,69,72,76,73]
+    print(dailyTemperatures(temperatures))
+
+    temperatures = [30,40,50,60]
+    print(dailyTemperatures(temperatures))
+
+    temperatures = [30,60,90]
+    print(dailyTemperatures(temperatures))
